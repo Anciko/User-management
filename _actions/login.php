c80c273a0fe1436456e4b8e072b7c0f7ed3785f5
Nyein Chan <konyeinchanko.it@gmail.com>
2022-07-09 23:17:54 +0630

    myfirstcommit

---
+++ _actions/login.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+include "../vendor/autoload.php";
+use Helpers\HTTP;
+use Libs\Database\MySQL;
+use Libs\Database\UsersTable;
+
+$email = $_POST['email'];
+$password = md5($_POST['password']);
+$table = new UsersTable(new MySQL());
+$user = $table->findByEmailAndPasword($email, $password);
+
+if ($user) {
+    if ($table->suspend($user->id)) {
+        HTTP::redirect("/index.php", "suspended=1");
+    }
+    $_SESSION['user'] = $user;
+    HTTP::redirect("/profile.php");
+} else {
+    HTTP::redirect("/index.php", "incorrect=1");
+}
